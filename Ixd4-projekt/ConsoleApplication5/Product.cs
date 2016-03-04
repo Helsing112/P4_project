b3a4e9b82aa8cc70802850b27ed6f4496a6939b5
Helsing112 <helsing112@hotmail.com>
2016-03-04 09:57:19 +0100

    aa

--- Ixd4-projekt/ConsoleApplication5/Product.cs
+++ Ixd4-projekt/ConsoleApplication5/Product.cs
@@ -8,8 +8,29 @@ namespace ConsoleApplication5
 {
     public class Product
     {
-        public decimal Price { get; set; }
+        private bool _Is_set = false;
+        private decimal _price;
+        /// <summary>
+        /// Boolean that determines wether price has been set
+        /// </summary>
+        public bool Is_set { get { return _Is_set; } }
+        public decimal Price
+        {
+            get { return _price; }
+            set { _price = value; _Is_set = true; }
+        }
 
         public string Name { get; set; }
+
+        public List<Product> Varianter = new List<Product>();
+
+        public void Add_to_Varrianter_list(string name, decimal price)
+        {
+            Product new_product = new Product();
+            new_product.Name = name;
+            new_product.Price = price;
+
+            Varianter.Add(new_product);
+        }
     }
 }
