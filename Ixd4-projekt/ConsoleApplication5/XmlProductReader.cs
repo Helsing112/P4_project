6c8a36d0a943d780e562886121f43054d219cabd
Helsing112 <helsing112@hotmail.com>
2016-03-04 09:56:19 +0100

    XmlProductReader

---
+++ Ixd4-projekt/ConsoleApplication5/XmlProductReader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace ConsoleApplication5
+{
+    public class XmlProductReader
+    {
+        public XmlProductReader()
+        {
+        }
+        /// <summary>
+        /// Reads a product file from XML. And returns a product instance
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public T Read_product<T>(string path) where T : Product
+        {
+            T TT;
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            StreamReader reader = new StreamReader(path);
+
+            TT = (T)serializer.Deserialize(reader);
+            reader.Close();
+
+            return TT;
+        }
+    }
+}
