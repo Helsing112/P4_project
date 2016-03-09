9f7572e18ac5c54cccb761f2141722a12e1f8d07
kranzekage <sunejohansson1992@gmail.com>
2016-03-09 12:17:40 +0100

    Kan nu tilføje billeder til produkter og stien bliver gemt i xml filerne.

--- Ixd4-projekt/ConsoleApplication5/Windows_Forms/XmlFileAdder.Designer.cs
+++ Ixd4-projekt/ConsoleApplication5/Windows_Forms/XmlFileAdder.Designer.cs
@@ -38,22 +38,28 @@
             this.label5 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.Create_cat_but = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // Text_input
             // 
-            this.Text_input.Location = new System.Drawing.Point(210, 114);
+            this.Text_input.Location = new System.Drawing.Point(236, 142);
+            this.Text_input.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Text_input.Name = "Text_input";
-            this.Text_input.Size = new System.Drawing.Size(328, 22);
+            this.Text_input.Size = new System.Drawing.Size(368, 26);
             this.Text_input.TabIndex = 0;
             this.Text_input.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(46, 114);
+            this.label1.Location = new System.Drawing.Point(52, 142);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(149, 17);
+            this.label1.Size = new System.Drawing.Size(168, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "Enter name of product";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -61,9 +67,10 @@
             // button1
             // 
             this.button1.Enabled = false;
-            this.button1.Location = new System.Drawing.Point(184, 215);
+            this.button1.Location = new System.Drawing.Point(207, 493);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(173, 36);
+            this.button1.Size = new System.Drawing.Size(195, 45);
             this.button1.TabIndex = 2;
             this.button1.Text = "Create product";
             this.button1.UseVisualStyleBackColor = true;
@@ -72,17 +79,18 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(46, 163);
+            this.label2.Location = new System.Drawing.Point(52, 204);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(77, 17);
+            this.label2.Size = new System.Drawing.Size(86, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "Enter price";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(210, 163);
+            this.textBox1.Location = new System.Drawing.Point(236, 204);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(328, 22);
+            this.textBox1.Size = new System.Drawing.Size(368, 26);
             this.textBox1.TabIndex = 4;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
             // 
@@ -90,9 +98,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.label3.Location = new System.Drawing.Point(179, 44);
+            this.label3.Location = new System.Drawing.Point(201, 55);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(182, 29);
+            this.label3.Size = new System.Drawing.Size(214, 36);
             this.label3.TabIndex = 5;
             this.label3.Text = "Create Product";
             // 
@@ -100,45 +108,77 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.label4.Location = new System.Drawing.Point(179, 288);
+            this.label4.Location = new System.Drawing.Point(201, 553);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(197, 29);
+            this.label4.Size = new System.Drawing.Size(230, 36);
             this.label4.TabIndex = 6;
             this.label4.Text = "Create Category";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(46, 362);
+            this.label5.Location = new System.Drawing.Point(52, 645);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(156, 17);
+            this.label5.Size = new System.Drawing.Size(175, 20);
             this.label5.TabIndex = 8;
             this.label5.Text = "Enter name of category";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(210, 362);
+            this.textBox2.Location = new System.Drawing.Point(236, 645);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(328, 22);
+            this.textBox2.Size = new System.Drawing.Size(368, 26);
             this.textBox2.TabIndex = 7;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // Create_cat_but
             // 
             this.Create_cat_but.Enabled = false;
-            this.Create_cat_but.Location = new System.Drawing.Point(184, 425);
+            this.Create_cat_but.Location = new System.Drawing.Point(207, 724);
+            this.Create_cat_but.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Create_cat_but.Name = "Create_cat_but";
-            this.Create_cat_but.Size = new System.Drawing.Size(173, 36);
+            this.Create_cat_but.Size = new System.Drawing.Size(195, 45);
             this.Create_cat_but.TabIndex = 9;
             this.Create_cat_but.Text = "Create Category";
             this.Create_cat_but.UseVisualStyleBackColor = true;
             this.Create_cat_but.Click += new System.EventHandler(this.Create_cat_but_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.ImageLocation = "@\"C:\\Users\\kranzekage\\Source\\Repos\\P4_project\\PictureLibrary\"";
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(207, 331);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(195, 155);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(207, 275);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(195, 50);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Add Picture";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // XmlFileAdder
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(565, 554);
+            this.ClientSize = new System.Drawing.Size(636, 815);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.Create_cat_but);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox2);
@@ -149,8 +189,10 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Text_input);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "XmlFileAdder";
             this.Text = "XmlFileAdder";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -168,5 +210,11 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button Create_cat_but;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+
+        
     }
 }
