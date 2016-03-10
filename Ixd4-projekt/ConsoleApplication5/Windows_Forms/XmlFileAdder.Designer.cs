d2f8da5c6e2f2e5186a7ebb5482f847e874b2a35
MortenLinnet <mortenlinnet@hotmail.com>
2016-03-10 10:42:26 +0100

    kk

--- Ixd4-projekt/ConsoleApplication5/Windows_Forms/XmlFileAdder.Designer.cs
+++ Ixd4-projekt/ConsoleApplication5/Windows_Forms/XmlFileAdder.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(XmlFileAdder));
             this.Text_input = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
@@ -41,26 +40,27 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button2 = new System.Windows.Forms.Button();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.CategoryTextBox = new System.Windows.Forms.TextBox();
             this.Create_Category_Button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // Text_input
             // 
-            this.Text_input.Location = new System.Drawing.Point(236, 142);
-            this.Text_input.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Text_input.Location = new System.Drawing.Point(157, 92);
+            this.Text_input.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Text_input.Name = "Text_input";
-            this.Text_input.Size = new System.Drawing.Size(368, 26);
+            this.Text_input.Size = new System.Drawing.Size(247, 20);
             this.Text_input.TabIndex = 0;
             this.Text_input.TextChanged += new System.EventHandler(this.ProductNameTextbox_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(52, 142);
+            this.label1.Location = new System.Drawing.Point(35, 92);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(168, 20);
+            this.label1.Size = new System.Drawing.Size(112, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Enter name of product";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -68,10 +68,10 @@
             // button1
             // 
             this.button1.Enabled = false;
-            this.button1.Location = new System.Drawing.Point(207, 493);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Location = new System.Drawing.Point(184, 277);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(195, 45);
+            this.button1.Size = new System.Drawing.Size(130, 29);
             this.button1.TabIndex = 2;
             this.button1.Text = "Create product";
             this.button1.UseVisualStyleBackColor = true;
@@ -80,17 +80,19 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(52, 204);
+            this.label2.Location = new System.Drawing.Point(35, 124);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.Size = new System.Drawing.Size(58, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Enter price";
             // 
-            // textBox1
+            // PriceInputTextBox
             // 
-            this.PriceInputTextBox.Location = new System.Drawing.Point(210, 163);
-            this.PriceInputTextBox.Name = "textBox1";
-            this.PriceInputTextBox.Size = new System.Drawing.Size(328, 22);
+            this.PriceInputTextBox.Location = new System.Drawing.Point(157, 117);
+            this.PriceInputTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.PriceInputTextBox.Name = "PriceInputTextBox";
+            this.PriceInputTextBox.Size = new System.Drawing.Size(247, 20);
             this.PriceInputTextBox.TabIndex = 4;
             this.PriceInputTextBox.TextChanged += new System.EventHandler(this.PriceTextBox_TextChanged_1);
             // 
@@ -98,9 +100,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.label3.Location = new System.Drawing.Point(201, 55);
+            this.label3.Location = new System.Drawing.Point(134, 36);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(214, 36);
+            this.label3.Size = new System.Drawing.Size(143, 25);
             this.label3.TabIndex = 5;
             this.label3.Text = "Create Product";
             // 
@@ -108,50 +111,34 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.label4.Location = new System.Drawing.Point(201, 553);
+            this.label4.Location = new System.Drawing.Point(170, 327);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(230, 36);
+            this.label4.Size = new System.Drawing.Size(156, 25);
             this.label4.TabIndex = 6;
             this.label4.Text = "Create Category";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(52, 645);
+            this.label5.Location = new System.Drawing.Point(17, 354);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(175, 20);
+            this.label5.Size = new System.Drawing.Size(117, 13);
             this.label5.TabIndex = 8;
             this.label5.Text = "Enter name of category";
-            // 
-            // textBox2
-            // 
-            this.CategoryTextBox.Location = new System.Drawing.Point(210, 362);
-            this.CategoryTextBox.Name = "textBox2";
-            this.CategoryTextBox.Size = new System.Drawing.Size(328, 22);
-            this.CategoryTextBox.TabIndex = 7;
-            this.CategoryTextBox.TextChanged += new System.EventHandler(this.CategoryTextbox_TextChanged);
-            // 
-            // Create_cat_but
-            // 
-            this.Create_Category_Button.Enabled = false;
-            this.Create_Category_Button.Location = new System.Drawing.Point(184, 425);
-            this.Create_Category_Button.Name = "Create_cat_but";
-            this.Create_Category_Button.Size = new System.Drawing.Size(173, 36);
-            this.Create_Category_Button.TabIndex = 9;
-            this.Create_Category_Button.Text = "Create Category";
-            this.Create_Category_Button.UseVisualStyleBackColor = true;
-            this.Create_Category_Button.Click += new System.EventHandler(this.Create_Category_Button_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.ErrorImage = global::ConsoleApplication5.Properties.Resources.placeholder;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Image = global::ConsoleApplication5.Properties.Resources.placeholder;
             this.pictureBox1.ImageLocation = "@\"C:\\Users\\kranzekage\\Source\\Repos\\P4_project\\PictureLibrary\"";
             this.pictureBox1.InitialImage = null;
-            this.pictureBox1.Location = new System.Drawing.Point(207, 331);
+            this.pictureBox1.Location = new System.Drawing.Point(184, 171);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(195, 155);
+            this.pictureBox1.Size = new System.Drawing.Size(130, 101);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 12;
             this.pictureBox1.TabStop = false;
@@ -164,19 +151,41 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(207, 275);
+            this.button2.Location = new System.Drawing.Point(184, 141);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(195, 50);
+            this.button2.Size = new System.Drawing.Size(130, 32);
             this.button2.TabIndex = 11;
             this.button2.Text = "Add Picture";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // CategoryTextBox
+            // 
+            this.CategoryTextBox.Location = new System.Drawing.Point(138, 354);
+            this.CategoryTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.CategoryTextBox.Name = "CategoryTextBox";
+            this.CategoryTextBox.Size = new System.Drawing.Size(220, 20);
+            this.CategoryTextBox.TabIndex = 7;
+            this.CategoryTextBox.TextChanged += new System.EventHandler(this.CategoryTextbox_TextChanged);
+            // 
+            // Create_Category_Button
+            // 
+            this.Create_Category_Button.Enabled = false;
+            this.Create_Category_Button.Location = new System.Drawing.Point(184, 387);
+            this.Create_Category_Button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Create_Category_Button.Name = "Create_Category_Button";
+            this.Create_Category_Button.Size = new System.Drawing.Size(115, 23);
+            this.Create_Category_Button.TabIndex = 9;
+            this.Create_Category_Button.Text = "Create Category";
+            this.Create_Category_Button.UseVisualStyleBackColor = true;
+            this.Create_Category_Button.Click += new System.EventHandler(this.Create_Category_Button_Click);
+            // 
             // XmlFileAdder
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(636, 815);
+            this.ClientSize = new System.Drawing.Size(424, 482);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.Create_Category_Button);
@@ -189,7 +198,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Text_input);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "XmlFileAdder";
             this.Text = "XmlFileAdder";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
