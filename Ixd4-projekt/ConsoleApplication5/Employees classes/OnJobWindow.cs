007d36a8ec9fefb6412aa4160868957fc667e629
MortenLinnet <mortenlinnet@hotmail.com>
2016-03-21 09:44:10 +0100

    jesa

---
+++ Ixd4-projekt/ConsoleApplication5/Employees classes/OnJobWindow.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApplication5
+{
+    public partial class OnJobWindow : Form
+    {
+        public OnJobWindow(string path)
+        {
+            InitializeComponent();
+            Initializejobpeople(path);  
+        }
+
+        private void Initializejobpeople(string path)
+        {
+            EmployeesReader EmployeeControl = new EmployeesReader(500, 200, path);
+            EmployeeControl.Location = new Point(0, 0  );
+            EmployeeControl.BorderStyle = BorderStyle.Fixed3D;
+
+            Controls.Add(EmployeeControl);
+        }
+
+        private void OnJobWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
