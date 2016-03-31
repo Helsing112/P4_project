ce8e0142e34e2128e1f4475e883a35fd3db76140
kranzekage <sunejohansson1992@gmail.com>
2016-03-31 14:01:14 +0200

    ekstra filer

---
+++ Ixd4-projekt/ConsoleApplication5/Standard layouts/Buttons/Back_Button.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApplication5.Standard_layouts.Buttons
+{
+    public partial class Back_Button : Std_Button
+    {
+        public Back_Button()
+        {
+            InitializeComponent();
+        }
+    }
+}
