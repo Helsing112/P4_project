6299cf1ff69c25fe0ef8e5cb748e4ac2f8c7ce92
kranzekage <sunejohansson1992@gmail.com>
2016-04-05 11:14:08 +0200

    Textbox

---
+++ Ixd4-projekt/ConsoleApplication5/Standard layouts/Buttons/TextBox_Touch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ConsoleApplication5.Standard_layouts.Buttons
+{
+    /// <summary>
+    /// Basic textbox layout, that includes the opening of the touch keyboard 
+    /// </summary>
+    public partial class TextBox_Touch : UserControl
+    {
+        public TextBox_Touch()
+        {
+            InitializeComponent();
+            this.textBox1.Click += new System.EventHandler(this.textbox_Click);
+            //this.textBox1.MouseLeave += new System.EventHandler(this.textbox_Focus);
+        }
+        // Opens the touch keyboard
+        private void textbox_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"C:\Program Files\Common Files\Microsoft Shared\ink\TabTip.exe");
+
+        }
+        // Kills the onscreen keyboard when mouse leaves the textbox 
+        //private void textbox_Focus(object sender, EventArgs e)
+        //{
+        //    Process[] oskProcess = Process.GetProcessesByName("TabTip");
+        //    foreach (Process process in oskProcess)
+        //    {
+        //        process.Kill();
+        //    }
+        //}
+
+    }
+}
