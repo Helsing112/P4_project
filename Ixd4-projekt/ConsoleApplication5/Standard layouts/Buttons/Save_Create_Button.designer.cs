6c9983adaf0002754069dca655bcb507b233ec93
kranzekage <sunejohansson1992@gmail.com>
2016-04-28 09:17:21 +0200

    filer ordnet lolz

---
+++ Ixd4-projekt/ConsoleApplication5/Standard layouts/Buttons/Save_Create_Button.designer.cs
@@ -0,0 +1,58 @@
+﻿namespace ConsoleApplication5
+{
+    partial class Save_Create_Button
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pictureBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1C:\Users\Morten\Source\Repos\P4_project\Ixd4-projekt\ConsoleApplication5\Standard layouts\Buttons\Numpad_button.cs
+            // 
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 13F, System.Drawing.FontStyle.Bold);
+            this.label1.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ConsoleApplication5.Properties.Resources.knap2;
+            // 
+            // Save_Create_Button
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "Save_Create_Button";
+            this.Textlabel = "Save / Accept";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pictureBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
