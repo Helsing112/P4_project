74f96f118e4424cc8f6e688673bbb8458c485cd3
Helsing112 <helsing112@hotmail.com>
2016-04-28 12:49:11 +0200

    aa

--- Ixd4-projekt/ConsoleApplication5/Standard layouts/Super classes/Std_Button.Designer.cs
+++ Ixd4-projekt/ConsoleApplication5/Standard layouts/Super classes/Std_Button.Designer.cs
@@ -1,4 +1,4 @@
-﻿ namespace ConsoleApplication5
+﻿namespace ConsoleApplication5
 {
     partial class Std_Button
     {
@@ -41,8 +41,9 @@
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(128, 34);
+            this.label1.Size = new System.Drawing.Size(144, 42);
             this.label1.TabIndex = 0;
+            this.label1.Text = "";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
             this.label1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
@@ -53,24 +54,23 @@
             this.pictureBox1.Controls.Add(this.label1);
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(128, 34);
+            this.pictureBox1.Size = new System.Drawing.Size(144, 42);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
             this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
             // 
-            // Std_Button
+            // Std_Layout
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.Load += Std_Layout_Load;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Transparent;
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.Name = "Std_Button";
-            this.Size = new System.Drawing.Size(128, 34);
+            this.Name = "Std_Layout";
+            this.Size = new System.Drawing.Size(144, 42);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.pictureBox1.ResumeLayout(false);
             this.ResumeLayout(false);
