3f346b8266d76a33e31350bd259fc20ff8c1014f
Helsing112 <helsing112@hotmail.com>
2016-04-29 09:43:53 +0200

    asd

--- Ixd4-projekt/ConsoleApplication5/TEST.Designer.cs
+++ Ixd4-projekt/ConsoleApplication5/TEST.Designer.cs
@@ -53,14 +53,14 @@ namespace ConsoleApplication5
             this.panel1.Controls.Add(this.Label_TableName);
             this.panel1.Location = new System.Drawing.Point(28, 62);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(531, 61);
+            this.panel1.Size = new System.Drawing.Size(435, 61);
             this.panel1.TabIndex = 12;
             // 
             // Button_BackToTable
             // 
             this.Button_BackToTable.BackColor = System.Drawing.Color.Transparent;
             this.Button_BackToTable.Image_set = null;
-            this.Button_BackToTable.Location = new System.Drawing.Point(1710, 963);
+            this.Button_BackToTable.Location = new System.Drawing.Point(1327, 769);
             this.Button_BackToTable.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Button_BackToTable.Name = "Button_BackToTable";
             this.Button_BackToTable.Size = new System.Drawing.Size(187, 79);
@@ -83,7 +83,7 @@ namespace ConsoleApplication5
             // 
             this.Button_Done.BackColor = System.Drawing.Color.Transparent;
             this.Button_Done.Image_set = null;
-            this.Button_Done.Location = new System.Drawing.Point(288, 808);
+            this.Button_Done.Location = new System.Drawing.Point(213, 614);
             this.Button_Done.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Button_Done.Name = "Button_Done";
             this.Button_Done.Size = new System.Drawing.Size(250, 105);
@@ -94,10 +94,10 @@ namespace ConsoleApplication5
             // 
             this.Button_Comment.BackColor = System.Drawing.Color.Transparent;
             this.Button_Comment.Image_set = null;
-            this.Button_Comment.Location = new System.Drawing.Point(28, 808);
+            this.Button_Comment.Location = new System.Drawing.Point(28, 614);
             this.Button_Comment.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Button_Comment.Name = "Button_Comment";
-            this.Button_Comment.Size = new System.Drawing.Size(250, 105);
+            this.Button_Comment.Size = new System.Drawing.Size(164, 105);
             this.Button_Comment.TabIndex = 10;
             this.Button_Comment.Textlabel = "COMMENT";
             // 
@@ -105,10 +105,10 @@ namespace ConsoleApplication5
             // 
             this.Button_PrintBill.BackColor = System.Drawing.Color.Transparent;
             this.Button_PrintBill.Image_set = null;
-            this.Button_PrintBill.Location = new System.Drawing.Point(28, 917);
+            this.Button_PrintBill.Location = new System.Drawing.Point(28, 723);
             this.Button_PrintBill.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Button_PrintBill.Name = "Button_PrintBill";
-            this.Button_PrintBill.Size = new System.Drawing.Size(250, 105);
+            this.Button_PrintBill.Size = new System.Drawing.Size(164, 105);
             this.Button_PrintBill.TabIndex = 9;
             this.Button_PrintBill.Textlabel = "PRINT";
             // 
@@ -116,9 +116,9 @@ namespace ConsoleApplication5
             // 
             this.Label_HeadLineFood.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold);
             this.Label_HeadLineFood.ForeColor = System.Drawing.Color.Black;
-            this.Label_HeadLineFood.Location = new System.Drawing.Point(587, 75);
+            this.Label_HeadLineFood.Location = new System.Drawing.Point(466, 74);
             this.Label_HeadLineFood.Name = "Label_HeadLineFood";
-            this.Label_HeadLineFood.Size = new System.Drawing.Size(647, 35);
+            this.Label_HeadLineFood.Size = new System.Drawing.Size(498, 35);
             this.Label_HeadLineFood.TabIndex = 8;
             this.Label_HeadLineFood.Text = "FOOD";
             this.Label_HeadLineFood.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -127,9 +127,9 @@ namespace ConsoleApplication5
             // 
             this.Label_HeadlineDrinks.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold);
             this.Label_HeadlineDrinks.ForeColor = System.Drawing.Color.Black;
-            this.Label_HeadlineDrinks.Location = new System.Drawing.Point(1250, 75);
+            this.Label_HeadlineDrinks.Location = new System.Drawing.Point(1016, 75);
             this.Label_HeadlineDrinks.Name = "Label_HeadlineDrinks";
-            this.Label_HeadlineDrinks.Size = new System.Drawing.Size(647, 35);
+            this.Label_HeadlineDrinks.Size = new System.Drawing.Size(498, 35);
             this.Label_HeadlineDrinks.TabIndex = 7;
             this.Label_HeadlineDrinks.Text = "DRINKS";
             this.Label_HeadlineDrinks.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -138,7 +138,7 @@ namespace ConsoleApplication5
             // 
             this.Pay_Button.BackColor = System.Drawing.Color.Transparent;
             this.Pay_Button.Image_set = null;
-            this.Pay_Button.Location = new System.Drawing.Point(288, 917);
+            this.Pay_Button.Location = new System.Drawing.Point(213, 723);
             this.Pay_Button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Pay_Button.Name = "Pay_Button";
             this.Pay_Button.Size = new System.Drawing.Size(250, 105);
@@ -150,42 +150,42 @@ namespace ConsoleApplication5
             this.tempReceipt1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.tempReceipt1.Location = new System.Drawing.Point(28, 120);
             this.tempReceipt1.Name = "tempReceipt1";
-            this.tempReceipt1.Size = new System.Drawing.Size(531, 666);
+            this.tempReceipt1.Size = new System.Drawing.Size(435, 489);
             this.tempReceipt1.TabIndex = 4;
             // 
             // ProductView_Foods
             // 
             this.ProductView_Foods.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.ProductView_Foods.Location = new System.Drawing.Point(587, 120);
+            this.ProductView_Foods.Location = new System.Drawing.Point(1016, 120);
             this.ProductView_Foods.Margin = new System.Windows.Forms.Padding(0);
             this.ProductView_Foods.Name = "ProductView_Foods";
-            this.ProductView_Foods.Size = new System.Drawing.Size(647, 814);
+            this.ProductView_Foods.Size = new System.Drawing.Size(498, 623);
             this.ProductView_Foods.TabIndex = 3;
             // 
             // ProductView_Drinks
             // 
             this.ProductView_Drinks.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.ProductView_Drinks.Location = new System.Drawing.Point(1250, 120);
+            this.ProductView_Drinks.Location = new System.Drawing.Point(466, 120);
             this.ProductView_Drinks.Margin = new System.Windows.Forms.Padding(0);
             this.ProductView_Drinks.Name = "ProductView_Drinks";
-            this.ProductView_Drinks.Size = new System.Drawing.Size(647, 814);
+            this.ProductView_Drinks.Size = new System.Drawing.Size(498, 699);
             this.ProductView_Drinks.TabIndex = 2;
             // 
             // timerInfo1
             // 
+            this.timerInfo1.BackColor = System.Drawing.Color.White;
             this.timerInfo1.Dock = System.Windows.Forms.DockStyle.Top;
             this.timerInfo1.employeename = null;
             this.timerInfo1.Location = new System.Drawing.Point(0, 0);
             this.timerInfo1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.timerInfo1.Name = "timerInfo1";
-            this.timerInfo1.Size = new System.Drawing.Size(1920, 55);
+            this.timerInfo1.Size = new System.Drawing.Size(1536, 50);
             this.timerInfo1.TabIndex = 1;
             this.timerInfo1.tablename = null;
             // 
             // TEST
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.Controls.Add(this.Button_BackToTable);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.Button_Done);
@@ -199,7 +199,7 @@ namespace ConsoleApplication5
             this.Controls.Add(this.ProductView_Drinks);
             this.Controls.Add(this.timerInfo1);
             this.Name = "TEST";
-            this.Size = new System.Drawing.Size(1920, 1080);
+            this.Size = new System.Drawing.Size(1536, 864);
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
