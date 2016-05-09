df098c8bdd0f823ac202c7f59c9fc0273071883a
Helsing112 <helsing112@hotmail.com>
2016-05-09 10:22:50 +0200

    b

--- Ixd4-projekt/ConsoleApplication5/Product And Category/SuperClassProductViewer.Designer.cs
+++ Ixd4-projekt/ConsoleApplication5/Product And Category/SuperClassProductViewer.Designer.cs
@@ -31,14 +31,15 @@ namespace ConsoleApplication5
         {
             this.Flow_ProductsFlow = new System.Windows.Forms.FlowLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.back_Button1 = new ConsoleApplication5.Button_BackArrow();
             this.Label_Headline = new ConsoleApplication5.Standard_Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // Flow_ProductsFlow
             // 
-            this.Flow_ProductsFlow.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.Flow_ProductsFlow.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Flow_ProductsFlow.AutoScroll = true;
             this.Flow_ProductsFlow.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
@@ -49,6 +50,7 @@ namespace ConsoleApplication5
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.back_Button1);
             this.panel1.Controls.Add(this.Label_Headline);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
@@ -57,10 +59,22 @@ namespace ConsoleApplication5
             this.panel1.TabIndex = 4;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
+            // back_Button1
+            // 
+            this.back_Button1.BackColor = System.Drawing.Color.Transparent;
+            this.back_Button1.Image_set = null;
+            this.back_Button1.Location = new System.Drawing.Point(30, 20);
+            this.back_Button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.back_Button1.Name = "back_Button1";
+            this.back_Button1.Size = new System.Drawing.Size(85, 47);
+            this.back_Button1.TabIndex = 0;
+            this.back_Button1.Textlabel = "";
+            this.back_Button1.Load += new System.EventHandler(this.back_Button1_Load);
+            // 
             // Label_Headline
             // 
-            this.Label_Headline.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.Label_Headline.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Label_Headline.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold);
             this.Label_Headline.ForeColor = System.Drawing.Color.Black;
@@ -87,6 +101,7 @@ namespace ConsoleApplication5
         #endregion
 
         private FlowLayoutPanel Flow_ProductsFlow;
+        public Button_BackArrow back_Button1;
         private Standard_Label Label_Headline;
         private Panel panel1;
     }
--- Ixd4-projekt/ConsoleApplication5/TablesPage.Designer.cs
+++ Ixd4-projekt/ConsoleApplication5/TablesPage.Designer.cs
@@ -45,19 +45,20 @@
             this.panel1.Controls.Add(this.Button_MoveContent);
             this.panel1.Controls.Add(this.Button_MoveTable);
             this.panel1.Controls.Add(this.Button_CombineTables);
-            this.panel1.Location = new System.Drawing.Point(13, 222);
+            this.panel1.Location = new System.Drawing.Point(9, 142);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(255, 802);
+            this.panel1.Size = new System.Drawing.Size(170, 513);
             this.panel1.TabIndex = 8;
             // 
             // Button_OneTimePayment
             // 
             this.Button_OneTimePayment.BackColor = System.Drawing.Color.Transparent;
             this.Button_OneTimePayment.Image_set = null;
-            this.Button_OneTimePayment.Location = new System.Drawing.Point(0, 76);
-            this.Button_OneTimePayment.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Button_OneTimePayment.Location = new System.Drawing.Point(0, 49);
+            this.Button_OneTimePayment.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.Button_OneTimePayment.Name = "Button_OneTimePayment";
-            this.Button_OneTimePayment.Size = new System.Drawing.Size(250, 138);
+            this.Button_OneTimePayment.Size = new System.Drawing.Size(167, 88);
             this.Button_OneTimePayment.TabIndex = 7;
             this.Button_OneTimePayment.Textlabel = "QUICK BILL";
             // 
@@ -65,10 +66,10 @@
             // 
             this.Button_MoveContent.BackColor = System.Drawing.Color.Transparent;
             this.Button_MoveContent.Image_set = null;
-            this.Button_MoveContent.Location = new System.Drawing.Point(0, 644);
-            this.Button_MoveContent.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Button_MoveContent.Location = new System.Drawing.Point(0, 412);
+            this.Button_MoveContent.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Button_MoveContent.Name = "Button_MoveContent";
-            this.Button_MoveContent.Size = new System.Drawing.Size(250, 138);
+            this.Button_MoveContent.Size = new System.Drawing.Size(167, 88);
             this.Button_MoveContent.TabIndex = 4;
             this.Button_MoveContent.Textlabel = "MOVE CONTENT";
             this.Button_MoveContent.Button_Click += new System.EventHandler(this.Button_MoveContent_Button_Click_1);
@@ -77,10 +78,10 @@
             // 
             this.Button_MoveTable.BackColor = System.Drawing.Color.Transparent;
             this.Button_MoveTable.Image_set = null;
-            this.Button_MoveTable.Location = new System.Drawing.Point(0, 500);
-            this.Button_MoveTable.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Button_MoveTable.Location = new System.Drawing.Point(0, 320);
+            this.Button_MoveTable.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Button_MoveTable.Name = "Button_MoveTable";
-            this.Button_MoveTable.Size = new System.Drawing.Size(250, 138);
+            this.Button_MoveTable.Size = new System.Drawing.Size(167, 88);
             this.Button_MoveTable.TabIndex = 5;
             this.Button_MoveTable.Textlabel = "MOVE TABLE";
             // 
@@ -88,10 +89,10 @@
             // 
             this.Button_CombineTables.BackColor = System.Drawing.Color.Transparent;
             this.Button_CombineTables.Image_set = null;
-            this.Button_CombineTables.Location = new System.Drawing.Point(0, 356);
-            this.Button_CombineTables.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Button_CombineTables.Location = new System.Drawing.Point(0, 228);
+            this.Button_CombineTables.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Button_CombineTables.Name = "Button_CombineTables";
-            this.Button_CombineTables.Size = new System.Drawing.Size(250, 138);
+            this.Button_CombineTables.Size = new System.Drawing.Size(167, 88);
             this.Button_CombineTables.TabIndex = 6;
             this.Button_CombineTables.Textlabel = "COMBINE TABLES";
             // 
@@ -100,10 +101,10 @@
             this.back_Button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.back_Button1.BackColor = System.Drawing.Color.Transparent;
             this.back_Button1.Image_set = null;
-            this.back_Button1.Location = new System.Drawing.Point(1365, 757);
-            this.back_Button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.back_Button1.Location = new System.Drawing.Point(1375, 775);
+            this.back_Button1.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.back_Button1.Name = "back_Button1";
-            this.back_Button1.Size = new System.Drawing.Size(231, 95);
+            this.back_Button1.Size = new System.Drawing.Size(154, 61);
             this.back_Button1.TabIndex = 2;
             this.back_Button1.Textlabel = "Back";
             // 
@@ -113,10 +114,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.table_Panel_for_MainPage1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.table_Panel_for_MainPage1.Location = new System.Drawing.Point(314, 89);
-            this.table_Panel_for_MainPage1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.table_Panel_for_MainPage1.Location = new System.Drawing.Point(209, 57);
             this.table_Panel_for_MainPage1.Name = "table_Panel_for_MainPage1";
-            this.table_Panel_for_MainPage1.Size = new System.Drawing.Size(1139, 781);
+            this.table_Panel_for_MainPage1.Size = new System.Drawing.Size(1152, 779);
             this.table_Panel_for_MainPage1.TabIndex = 1;
             // 
             // timerInfo1
@@ -125,24 +125,23 @@
             this.timerInfo1.Dock = System.Windows.Forms.DockStyle.Top;
             this.timerInfo1.employeename = null;
             this.timerInfo1.Location = new System.Drawing.Point(0, 0);
-            this.timerInfo1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.timerInfo1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.timerInfo1.Name = "timerInfo1";
-            this.timerInfo1.Size = new System.Drawing.Size(1536, 50);
+            this.timerInfo1.Size = new System.Drawing.Size(1546, 50);
             this.timerInfo1.TabIndex = 0;
             this.timerInfo1.tablename = null;
             // 
             // TablesPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Transparent;
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.back_Button1);
             this.Controls.Add(this.table_Panel_for_MainPage1);
             this.Controls.Add(this.timerInfo1);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "TablesPage";
-            this.Size = new System.Drawing.Size(1536, 864);
+            this.Size = new System.Drawing.Size(1546, 864);
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
