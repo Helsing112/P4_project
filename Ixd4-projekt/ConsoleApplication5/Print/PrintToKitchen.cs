374b3a7667972c05f1d9979c498e3531741d3641
Helsing112 <helsing112@hotmail.com>
2016-05-24 13:40:31 +0200

    Moved printer

--- Ixd4-projekt/ConsoleApplication5/Print/PrintToKitchen.cs
+++ Ixd4-projekt/ConsoleApplication5/Print/PrintToKitchen.cs
@@ -1,35 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Drawing.Printing;
-using System.Drawing;
-using System.IO;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Drawing.Printing;
+//using System.Drawing;
+//using System.IO;
 
 
-namespace ConsoleApplication5
-{
-    class PrintToKitchen
-    {
-        public string TableName { get; set; }
-        public string EmployeeName { get; set; }
-        public List<ReceiptProduct> ProductList { get; set; }
-        public string TextFromComment { get; set; }
-        public string Printername { get; set; }
+//namespace ConsoleApplication5
+//{
+//    class PrintToKitchen
+//    {
+//        public string TableName { get; set; }
+//        public string EmployeeName { get; set; }
+//        public List<ReceiptProduct> ProductList { get; set; }
+//        public string TextFromComment { get; set; }
+//        public string Printername { get; set; }
 
         
-        public PrintToKitchen(string Tablename, string employename, List<ReceiptProduct> Products, string textfromcomment, string printername)
-        {
-            this.TableName = Tablename;
-            this.ProductList = Products;
-            this.EmployeeName = employename;
-            this.TextFromComment = textfromcomment;
-            this.Printername = printername;
-        }
+//        public PrintToKitchen(string Tablename, string employename, List<ReceiptProduct> Products, string textfromcomment, string printername)
+//        {
+//            this.TableName = Tablename;
+//            this.ProductList = Products;
+//            this.EmployeeName = employename;
+//            this.TextFromComment = textfromcomment;
+//            this.Printername = printername;
+//        }
 
 
 
 
-    }
-}
+//    }
+//}
